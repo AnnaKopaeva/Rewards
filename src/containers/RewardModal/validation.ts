3a9dafaf641661c31b004e0cb3b3a2cb52aeb544
anna.havelia <anna.havelia@itrexgroup.com>
2021-10-26 16:03:06 +0300

    Deleted custom components and used material ui components

--- src/containers/RewardModal/validation.ts
+++ src/containers/RewardModal/validation.ts
@@ -2,6 +2,7 @@ import * as Yup from "yup";
 
 const MAX_LENGTH = 10;
 const MAX_ERROR_MESSAGE = `Should be less that ${MAX_LENGTH} symbols`;
+
 export const RewardSchema = Yup.object().shape({
   user: Yup.object().shape({ name: Yup.string() }).required("User is required").nullable(),
   reward: Yup.string().max(MAX_LENGTH, MAX_ERROR_MESSAGE).required("Reward is required"),
