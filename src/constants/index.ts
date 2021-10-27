44015d12d7648c01b70e700f334dea7d9e40d407
anna.havelia <anna.havelia@itrexgroup.com>
2021-10-27 20:16:05 +0300

    Separated components for profile and currency

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,7 @@ export enum SIZE {
   s = "s",
   m = "m",
 }
+
+export enum CURRENCY {
+  dollar = "$",
+}
--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -2,39 +2,38 @@ import * as React from "react";
 import { useSelector } from "react-redux";
 
 import Box from "@mui/material/Box";
-import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
 
-import { SIZE } from "../../constants";
-import Avatar from "../../components/Avatar";
-import { selectProfileData } from "../../store/profile/selectors";
+import Currency from "../../components/Currency";
+import Profile from "../Profile";
+
+import { CURRENCY } from "../../constants";
+
 import { selectTotalMyRewards, selectTotalGiveRewards } from "../../store/rewards/selectors";
 
 import styles from "./Header.styles";
 
 const Header = () => {
-  const profile = useSelector(selectProfileData);
   const myRewards = useSelector(selectTotalMyRewards);
   const giveRewards = useSelector(selectTotalGiveRewards);
 
   return (
     <Box sx={styles.header}>
-      <Box sx={styles.avatar}>
-        <Avatar size={SIZE.m} />
-        {profile ? (
-          <Typography sx={styles.name}>{profile.name}</Typography>
-        ) : (
-          <Skeleton sx={styles.name} />
-        )}
-      </Box>
+      <Profile />
       <Box sx={styles.details}>
         <Box sx={styles.block}>
           <Typography gutterBottom>My Rewards</Typography>
-          <Typography sx={styles.count}>{myRewards}$</Typography>
+          <Typography sx={styles.count}>
+            {myRewards}
+            <Currency value={CURRENCY.dollar} />
+          </Typography>
         </Box>
         <Box>
           <Typography gutterBottom>Give</Typography>
-          <Typography sx={styles.count}>{giveRewards}$</Typography>
+          <Typography sx={styles.count}>
+            {giveRewards}
+            <Currency value={CURRENCY.dollar} />
+          </Typography>
         </Box>
       </Box>
     </Box>
