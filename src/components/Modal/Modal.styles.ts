3583ffb848eafca6188924ed54d4a565802d6da5
anna.havelia <anna.havelia@itrexgroup.com>
2021-10-27 13:19:37 +0300

    Changed styles for avatar, header and empty panel

--- src/components/Modal/Modal.styles.ts
+++ src/components/Modal/Modal.styles.ts
@@ -1,9 +1,14 @@
-export const style = {
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4,
-};
+const styles = {
+  modal: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+  },
+} as const;
+
+export default styles;
