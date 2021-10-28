e135475d358910bed8bd8bdaeb0274f32a0502f9
anna.havelia <anna.havelia@itrexgroup.com>
2021-10-28 11:13:02 +0300

    Created wrapper for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
 import * as React from "react";
-import { useDispatch, useSelector } from "react-redux";
 
-import useToggleModal from "./hooks/useToggleModal";
 import Feed from "./containers/Feed";
 import Header from "./containers/Header";
-import RewardModal from "./containers/RewardModal";
-import Layout from "./components/Layout";
+import Wrapper from "./components/Wrapper";
 
-import { fetchUser } from "./store/users/actions";
-import { selectProfileLoaded } from "./store/profile/selectors";
-
-const App = () => {
-  const dispatch = useDispatch();
-
-  const isLoaded = useSelector(selectProfileLoaded);
-
-  React.useEffect(() => {
-    dispatch(fetchUser());
-  }, [dispatch]);
-
-  const { open, handleOpenModal, handleCloseModal } = useToggleModal();
-
-  return (
-    <Layout loading={!isLoaded}>
-      <Header />
-      <Feed onOpen={handleOpenModal} />
-      <RewardModal open={open} onClose={handleCloseModal} />
-    </Layout>
-  );
-};
+const App = () => (
+  <Wrapper>
+    <Header />
+    <Feed />
+  </Wrapper>
+);
 
 export default App;
