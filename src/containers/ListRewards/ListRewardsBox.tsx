c06554de25205fead133571efa5f37764f9346d9
anna.havelia <anna.havelia@itrexgroup.com>
2021-10-28 11:59:50 +0300

    Changed import

--- src/containers/ListRewards/ListRewardsBox.tsx
+++ src/containers/ListRewards/ListRewardsBox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import dayjs from "dayjs";
 import relativeTimePlugin from "dayjs/plugin/relativeTime";
 
--- src/containers/ListRewards/ListRewardsSkeleton.tsx
+++ src/containers/ListRewards/ListRewardsSkeleton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 
 import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
